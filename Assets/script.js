4f304a688e8ee2b412e819e0ae7e4488480876ae
Jadyn Gomez <jadyngg19@gmail.com>
2023-04-05 19:40:07 -0400

    Revise pricing functionality to tie in with event url

--- Assets/script.js
+++ Assets/script.js
@@ -3,10 +3,11 @@ const API_URL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${
 
 $(document).ready(() => {
   let radius = $("#radiusInput").val();
-  let type = $("#typeInput").val();
+	let type = $("#typeInput").val();
 
   $("#searchButton").click(() => {
     const userLocation = $("#locationInput").val();
+
     if (userLocation) {
       $.ajax({
         url: API_URL,
@@ -21,36 +22,40 @@ $(document).ready(() => {
         success: (response) => {
           const events = response._embedded.events;
           if (events.length === 0) {
-            $("#events").html("<p>No events found for the specified location.</p>");
+            $("#events").html("<p>No events found for the specified location and radius.</p>");
           } else {
             const eventsHtml = events.map((event) => {
+              let priceHtml = "<p>No pricing information available.</p>";
+              if (event.priceRanges && event.priceRanges.length > 0) {
+                const priceRange = event.priceRanges[0];
+                priceHtml = `<a href="${event.url}" target="_blank" class="event-price">Price: ${priceRange.min} - ${priceRange.max} ${priceRange.currency} (${priceRange.type})</a>`;
+              }
               return `
                 <h2 class="event-name">${event.name}</h2>
-                <p class="event-date">${event.dates.start.localDate} at ${event.dates.start.localTime}</p>
+                <p class="event.date">${event.dates.start.localDate} at ${event.dates.start.localTime}</p>
                 <p class="event-location">${event._embedded.venues[0].name}, ${event._embedded.venues[0].city.name}</p>
-                <p class="event-type">Type: ${event.classifications[0].segment.name}</p>
-                <img src="${event.images[0].url}" alt="${event.name}" class="event-image">
-                <a href="${event.url}" target="_blank" class="event-price">Buy Tickets</a>
-                <hr class="event-line">
+                <img class="event-image" src="${event.images[0].url}" alt="${event.name}">
+                ${priceHtml}
+                <hr class="event-line" />
+                <br />
               `;
             }).join("");
             $("#events").html(eventsHtml);
           }
         },
         error: (error) => {
-          console.log(error); 
+          console.log(error);
         }
       });
     }
-  });
-
-  $("#radiusInput").change(() => {
-    radius = $("#radiusInput").val();
-    $("#searchButton").trigger("click");
-  });
-
-  $("#typeInput").change(() => {
-    type = $("#typeInput").val();
-    $("#searchButton").trigger("click");
+    $("#radiusInput").change(() => {
+      radius = $("#radiusInput").val();
+      $("#searchButton").trigger("click");
+    });
+    
+    $("#typeInput").change(() => {
+      type = $("#typeInput").val();
+      $("#searchButton").trigger("click");
+    });
   });
 });
